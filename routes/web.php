1c1ea6872c6d94be756c77ae1ffd4df636ce332c
SeraphinaF <1028267@hr.nl>
2022-09-29 12:35:35 +0200

    installed login and registration

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::resource('projects', ProjectController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
